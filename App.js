852daf108aa7a121337d4ccf1f2f198b26c56bf7
AdhamSelim21 <161260432+AdhamSelim21@users.noreply.github.com>
2024-03-03 23:28:10 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,103 @@
+import { Button, Text, View } from "react-native";
+import { useState, useEffect, setInterval } from "react";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
+
+export default function App() {
+  const [expoPushToken, setExpoPushToken] = useState("");
+
+  useEffect(() => {
+    console.log("Registering for push notifications...");
+    registerForPushNotificationsAsync()
+      .then((token) => {
+        console.log("token: ", token);
+        setExpoPushToken(token);
+      })
+      .catch((err) => console.log(err));
+
+    // Create interval to send notification every hour
+   // const intervalId = ()=> setInterval(
+     // ()=>{      console.log("Sending notification every hour...");
+     // sendNotification();}
+    //, 10000); // 3600000 milliseconds are equivalent to 1 hour
+
+    // Cleanup function to clear the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, []);
+
+  async function registerForPushNotificationsAsync() {
+    let token;
+
+    if (Platform.OS === "android") {
+      await Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+    }
+
+    if (Device.isDevice) {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        alert("Failed to get push token for push notification!");
+        return;
+      }
+      token = (
+        await Notifications.getExpoPushTokenAsync({
+          projectId: "9106c95e-f516-43a9-b692-241fa7f6b5af",
+        })
+      ).data;
+      console.log(token);
+    } else {
+      alert("Must use physical device for Push Notifications");
+    }
+
+    return token;
+  }
+
+  const sendNotification = async () => {
+    console.log("Sending push notification...");
+
+    // notification message
+    const message = {
+      to: expoPushToken,
+      sound: "default",
+      title: "My first push notification!",
+      body: "This is my first push notification made with expo rn app",
+    };
+
+    await fetch("https://exp.host/--/api/v2/push/send", {
+      method: "POST",
+      headers: {
+        host: "exp.host",
+        accept: "application/json",
+        "accept-encoding": "gzip, deflate",
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(message),
+    });
+  };
+
+  return (
+    <View style={{ marginTop: 100, alignItems: "center" }}>
+      <Text style={{ marginVertical: 30 }}>adham</Text>
+      {/* Button removed as notifications are sent automatically */}
+      { <Button title="Send push notification" onPress={sendNotification} /> }
+    </View>
+  );
+}
